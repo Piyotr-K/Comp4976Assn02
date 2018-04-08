c7cf45f445598b16f72449359e6d921b93ec795c
Piyotr-K <piyotrkao1998@Outlook.com>
2018-04-07 17:10:03 -0700

    Initial Commit
    Signed-off-by: Piyotr-K <piyotrkao1998@Outlook.com>

---
+++ LymcWeb/Models/ApplicationUser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace LymcWeb.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(64)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(64)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        public string Street { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Province { get; set; }
+        [Required]
+        [Display(Name = "Postal Code")]
+        public string PostalCode { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string MobileNumber { get; set; }
+        public double SailingExperience { get; set; }
+
+        public ICollection<Boat> Boats { get; set; }
+    }
+}
---
+++ LymcWeb/Models/Boat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LymcWeb.Models
+{
+    public class Boat
+    {
+        public int BoatId { get; set; }
+
+        [Required]
+        public string BoatName { get; set; }
+
+        [Required]
+        public double LengthInFeet { get; set; }
+        public string Make { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        [Required]
+        [ScaffoldColumn(false)]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date Created")]
+        public DateTime CreationDate { get; set; } = DateTime.UtcNow;
+
+        [ForeignKey("User")]
+        [ScaffoldColumn(false)]
+        [Display(Name = "Created By")]
+        public string CreatedBy { get; set; }
+
+        public ApplicationUser User { get; set; }
+    }
+}
