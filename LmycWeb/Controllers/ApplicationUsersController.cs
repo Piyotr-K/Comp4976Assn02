04c6c98fc01edb16ce85c442d502416646e1b97c
Piyotr-K <piyotrkao1998@Outlook.com>
2018-04-08 22:32:58 -0700

    Ad
    Signed-off-by: Piyotr-K <piyotrkao1998@Outlook.com>

--- LmycWeb/Controllers/ApplicationUsersController.cs
+++ LmycWeb/Controllers/ApplicationUsersController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using LmycWeb.Data;
 using LmycWeb.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace LmycWeb.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class ApplicationUsersController : Controller
     {
         private readonly ApplicationDbContext _context;
